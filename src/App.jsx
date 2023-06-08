16106a3e017e555f4c0cdda910f6c44352d60ba9
NORDIN M'HAMED <sewasy678@gmail.com>
2023-06-08 14:18:54 +0200

    acabar please

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
-
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import { useState } from "react";
 function App() {
+  
+const [foods, setfoods] = useState([...foodsJson]);
+  const handleDelete = (id) => {
+    setfoods(foods.filter((f) => f.id !== id))
+    
+  }
+  const components = foods.map((f) =><FoodBox key={f.id} food={f} onDelete={handleDelete} /> )
+  
+  
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <ul className="ulgrid">{components}</ul>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+export default function FoodBox({ food, onDelete }) {
+    
+    return (
+        <div className="div">
+        <p>{food.name}</p>
+      
+        <img className="imgm"  src={food.image} />
+      
+        <p>Calories: {food.calories}</p>
+        <p>Servings {food.servings}</p>
+      
+        <p>
+          <b>
+            Total Calories: {food.servings} * {food.calories}{" "}
+          </b>{" "}
+          kcal
+        </p>
+      
+        <button onClick =  {() => onDelete(food.id)}>Delete</button>
+      </div>
+  );
+} // Your code here
